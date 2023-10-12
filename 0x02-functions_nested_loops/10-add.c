d612640c2fd49ac6b9846a825288b1d31be4170e
juniorisaacgithub <owinojuniorisaac@gmail.com>
2023-10-12 03:48:15 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,29 +1,14 @@
 #include "main.h"
 
-#include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
- * @n: The number to start printing from
- * Return: Always 0
+ * add - Adds twwo values
+ * @n: The first number to be computed
+ * @n: The second number to be computed
+ * Return: Integer value
  */
 
-void print_to_98(int n)
+int add(int n1, int n2)
 {
-	if (n <= 98)
-	{
-	for (; n >= 98; n++)
-	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
-	else
-	{
-	printf("%d, ", n);
-	}
-	}
-	}
+	return (n1 + n2);
 }

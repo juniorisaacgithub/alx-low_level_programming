1a25c4ade9786b26340d61e6bb576449ee6e3605
juniorisaacgithub <owinojuniorisaac@gmail.com>
2023-10-23 15:16:25 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _memcpy - function copies @n bytes from memory area @src
+ * _memcpy - function copies @n bytesfrom memory area @src
  * to memory area @dest
  *
  * @n: function copies
  *
  * @src: bytes from memory area
  *
- * @dest: to memory area 
+ * @dest: to memory area
  *
  * Return: a pointer to @dest
  */
@@ -21,6 +21,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
-	
+
 	return (dest);
 }

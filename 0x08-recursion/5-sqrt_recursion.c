e196feced3d9866e37d030b4017a0e823ee23a5b
juniorisaacgithub <owinojuniorisaac@gmail.com>
2023-10-25 15:42:13 -0700

    100-is_palindrome.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of 
+ * @n: number to calculate the square root of
  *
  * Return: the resulting square root
  */
@@ -26,6 +27,6 @@ int actual_sqrt_recursion(int n, int i)
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
-		return (i);
+		return (1);
 	return (actual_sqrt_recursion(n, i + 1));
 }
